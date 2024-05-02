52425e5071a67f6e3857671a318e92530d4decf9
Oirad7 <dario.aqn@gmail.com>
2024-05-02 17:15:04 -0300

    testes com tipos de variáveis

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500D;
+        long long = 16941900111;
+        float pi = 3.14F;
+        String nomeCompleto= "Dario Moura";
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        //fazendo cast
+        short numeroCurto2 = (short) numeroNormal;
+
+        boolean = true;
+
+        //constantes em CAIXA ALTA e palavra reservada final
+        final String FUNDACAO = "1975";
+
+
+    }
+}
